dcc02555ffeeecd688126d31a6dcab2e9ab12207
nidwbin <491216697@qq.com>
2021-11-29 23:37:11 +0800

    finished

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from signal.utils import Sampler, Recurrence
 from signal.tools import draw
 
 
-def formula1(n: int):
+def formula1(n: int or float):
     """
     x(n) = e^((-0.2+0.7j)n)
     x(n) = e^(-0.2n)*e^(0.7nj)
@@ -23,31 +23,38 @@ def formula2(n: int):
 
 
 if __name__ == '__main__':
-    step = Step(-10, 10, 2, delta_time=0.01, signal_type=float)
-    # start_time = -10
-    # end_time = 10
-    # impulse = Impulse(start_time, end_time)
-    # step = Step(start_time, end_time)
-    # signal1 = RealFormulaSignal(lambda x: 0.75 ** x, start_time, end_time)
-    # signal2 = PluralFormulaSignal(formula1, start_time, end_time)
-    # draw([impulse, step, signal1, signal2])
-    # signal3 = RealFormulaSignal(formula2, 0, 2, delta_time=0.01, signal_type=float)
-    # sample3 = Sampler(signal3, 32)
-    # t1, x1 = zip(*list(signal3))
-    # t2, x2 = zip(*list(sample3))
-    # pyplot.plot(t1, x1)
-    # pyplot.stem(t2, x2)
-    # pyplot.xlabel("时间")
-    # pyplot.ylabel("信号强度")
-    # pyplot.show()
-    #
-    # rec1 = Recurrence([1, 0.75, 0.125], [1, -1], impulse)
-    # rec2 = Recurrence([1, 0.75, 0.125], [1, -1], step)
-    #
-    # rec3 = Recurrence([1, -0.8], [0.15],
-    #                   RealFormulaSignal(lambda x: 2 * math.sin(0.05 * math.pi * x),
-    #                                     start_time, end_time)
-    #                   )
-    # signal4 = Step(-20, 20) - Step(-20, 20, switch_time=10)
-    # rec4 = Recurrence([1, -0.9], [1], signal4)
-    # draw([rec1, rec2, rec3, signal4, rec4])
+    draw([Impulse(-10, 10, 2), Impulse(-10, 10, 2, delta_time=0.01, signal_type=float)])
+
+    draw([Step(-10, 10, 2), Step(-10, 10, 2, delta_time=0.01, signal_type=float)])
+
+    draw([RealFormulaSignal(lambda x: 0.75 ** x, -10, 10),
+          RealFormulaSignal(lambda x: 0.75 ** x, -10, 10,delta_time=0.01, signal_type=float)])
+
+    PluralFormulaSignal(formula1, -10, 10).draw()
+
+    signal3 = RealFormulaSignal(formula2, 0, 2, delta_time=0.01, signal_type=float)
+    sample3 = Sampler(signal3, 32)
+    t1, x1 = zip(*list(signal3))
+    t2, x2 = zip(*list(sample3))
+    pyplot.plot(t1, x1)
+    pyplot.stem(t2, x2)
+    pyplot.xlabel("时间")
+    pyplot.ylabel("信号强度")
+    pyplot.show()
+
+    impulse = Impulse(-20, 20)
+    rec1 = Recurrence([1, 0.75, 0.125], [1, -1], impulse)
+    rec1.draw()
+
+    step = Step(-20, 20)
+    rec2 = Recurrence([1, 0.75, 0.125], [1, -1], step)
+    rec2.draw()
+
+    rec3 = Recurrence([1, -0.8], [0.15],
+                      RealFormulaSignal(lambda x: 2 * math.sin(0.05 * math.pi * x), -20, 20))
+    rec3.draw()
+
+    signal4 = Step(-20, 20) - Step(-20, 20, switch_time=10)
+    signal4.draw()
+    rec4 = Recurrence([1, -0.9], [1], signal4)
+    rec4.draw()
